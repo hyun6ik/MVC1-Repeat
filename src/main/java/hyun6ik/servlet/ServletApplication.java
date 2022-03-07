367d663e1f7ea06af3f222d558608bbc5f04b0c9
hyun6ik <fkanwm@naver.com>
2022-03-07 18:50:30 +0900

    Add #1 : 프로젝트 생성

---
+++ src/main/java/hyun6ik/servlet/ServletApplication.java
@@ -0,0 +1,13 @@
+package hyun6ik.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
+
+}
