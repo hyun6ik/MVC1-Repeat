b09ccc9308c2162f2c96e7938a78df7f50e81d36
hyun6ik <fkanwm@naver.com>
2022-03-10 17:13:02 +0900

    Add #4 : HelloServlet 및 welcome 페이지 구현

--- src/main/java/hyun6ik/servlet/basic/HelloServlet.java
+++ src/main/java/hyun6ik/servlet/basic/HelloServlet.java
@@ -12,7 +12,17 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        final String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
     }
 }
