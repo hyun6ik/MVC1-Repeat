a5ada894d0e9682846780765c07224b9c4ca2ee2
hyun6ik <fkanwm@naver.com>
2022-03-15 18:18:01 +0900

    Add #24 : 뷰 리졸버

--- src/main/java/hyun6ik/servlet/basic/web/springmvc/old/OldController.java
+++ src/main/java/hyun6ik/servlet/basic/web/springmvc/old/OldController.java
@@ -1,5 +1,6 @@
 package hyun6ik.servlet.basic.web.springmvc.old;
 
+import hyun6ik.servlet.basic.web.frontcontroller.ModelView;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
@@ -10,9 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
 
+    private static final String VIEW_NAME = "new-form";
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView(VIEW_NAME);
     }
 }
